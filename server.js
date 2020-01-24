918d11d30e99b4c9cdcd9ef340925ed120641755
Randy Gohmann <rgohma2@gmail.com>
2020-01-24 16:21:19 -0600

    set up server and renders home template

---
+++ server.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT
+
+
+
+app.get('/', (req, res) => {
+	res.render('home.ejs')	
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+	console.log('Server running on port: ' + PORT);
+})
