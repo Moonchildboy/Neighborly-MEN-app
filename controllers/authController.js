943a314a77dbce28dc708ef19be9d80bbe55dce4
Randy Gohmann <rgohma2@gmail.com>
2020-01-26 14:59:41 -0600

    can now see submitted input in register page in req.body

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/user')
+
+
 
 
 
@@ -8,6 +11,15 @@ router.get('/register', (req, res) => {
 	res.render('register.ejs')
 })
 
+
+
+
+
+
+router.post('/register', async (req, res, next) => {
+	console.log(req.body);
+	res.redirect('/auth/register')
+})
 
 
 
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 require('dotenv').config()
 const express = require('express')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 const app = express()
 const PORT = process.env.PORT
 
 
 
 
+
+
+// middleware
+app.use(express.static('public'))
+app.use(methodOverride('_method'))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 
 
