9ea1472b476724d5cbe014b197bc83958dd95aed
Darron B <dadonbay@gmail.com>
2020-01-27 14:25:17 -0600

    commiting to pull

--- controllers/postController.js
+++ controllers/postController.js
@@ -12,7 +12,7 @@ router.post('/', async (req, res, next) => {
 	console.log(req.body);
 	const createdPost = await Post.create(req.body)
 		// create date later
-	console.log("createdPost:",createdPost);
+	console.log("createdPost:", createdPost);
 	res.redirect('posts/')
 }) //end of create route
 
@@ -21,6 +21,10 @@ router.get('/', async (req, res, next) => {
 	const allPosts = await Post.find({})
   res.render('posts/index.ejs', {posts: allPosts})
 })// end of index route
+
+router.get('/', async (req, res, next) => {
+	
+}) //end of show route
 
 module.exports = router 
 
