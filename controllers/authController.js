f4b69545faebcafda9c766e82a8e30a4839a9c5d
Randy Gohmann <rgohma2@gmail.com>
2020-01-27 09:30:56 -0600

    added username taken logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,11 +14,22 @@ router.get('/register', (req, res) => {
 
 
 
-
-
 router.post('/register', async (req, res, next) => {
 	console.log(req.body);
+	const desiredUsername = req.session.username
+	const desiredPassword = req.session.password
+
+	const userWithUsername = await User.findOne({
+		username: desiredUsername
+	})
+
+	if (userWithUsername) {
+		console.log('username exists');
+	}
+	req.session.message = `Username ${desiredUsername} is taken.`
+	req.session.messageStatus = 'bad'
 	res.redirect('/auth/register')
+	
 })
 
 
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const session = require('express-session')
 const app = express()
 const PORT = process.env.PORT
 
