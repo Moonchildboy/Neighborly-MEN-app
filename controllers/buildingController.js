6d2fbca33a9f656d07b17244f90fd394546451df
Randy Gohmann <rgohma2@gmail.com>
2020-01-27 11:43:31 -0600

    added new buiding route

--- controllers/buildingController.js
+++ controllers/buildingController.js
@@ -3,6 +3,12 @@ const router = express.Router()
 const User = require('../models/building')
 
 
+router.get('/new', (req, res) => {
+	res.render('buildings/new.ejs')	
+})
+
+
+
 
 
 module.exports = router
