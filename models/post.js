2797e4b734d7358bdebb42811ef68d86b05e56af
Randy Gohmann <rgohma2@gmail.com>
2020-01-27 11:19:38 -0600

    added date to postSchema

--- models/post.js
+++ models/post.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose')
 const postSchema = new mongoose.Schema({
 	title: String,
 	content: String,
+	date: {
+		type: Date,
+		default: Date.now
+	},
 	// comments: [Comment.schema],
 	building: {
 		type: mongoose.Schema.Types.ObjectId,
