f931c2ca663e50c22a71a738f42013d1221e8c9a
Darron B <dadonbay@gmail.com>
2020-01-27 11:25:03 -0600

    commiting to pull

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const Post = require('../models/post')
 
 
 router.get('/', (req, res, next) => {
 	// const allPosts = await Post.find({}).populate('...')
   res.render('posts/index.ejs')
 })
+
+module.exports = router 
